1b4bc93fa449d789d45f5655f04452d7704df544
jimweng <jimweng01@hotmail.com>
2020-11-30 20:38:18 +0800

    add redux devtool

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import App from './components/App';
 import reducers from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
+    composeEnhancer(
     applyMiddleware(sagaMiddleware)
-)
+))
 
 sagaMiddleware.run(rootSaga)
 
